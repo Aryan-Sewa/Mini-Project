07e432cadb0541300dd5699e46df1188e9839e21
Aryan-Sewa <aryansewa2004@gmail.com>
2024-11-23 15:22:04 +0530

    signin function can check both user n trust

--- signin.py
+++ signin.py
@@ -4,12 +4,18 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 
 class Signin:
-    def __init__(self, json_file='users.json'):
+    def __init__(self, json_file='users.json', json_file2='trust.json'):
         self.json_file = json_file
+        self.json_file2 = json_file2
         print(f"Checking path: {self.json_file}")
+        print(f"Checking path: {self.json_file2}")
         print(f"File exists: {os.path.exists(self.json_file)}")
+        print(f"File exists: {os.path.exists(self.json_file2)}")
         if not os.path.exists(self.json_file):
             raise FileNotFoundError(f"The file {self.json_file} does not exist.")
+        
+        if not os.path.exists(self.json_file2):
+            raise FileNotFoundError(f"The file {self.json_file2} does not exist.")
     
     def check_signin(self, email, password):
         with open(self.json_file, 'r') as file:
@@ -19,7 +25,15 @@ class Signin:
                     if check_password_hash(user["password"], password):
                         return True, "Welcome back!"
                     else:
-                        return False, "Incorrect password."
-            return False, "No such user found."
+                        with open(self.json_file2, 'r') as file2:
+                            trusts = json.load(file2)
+                            for trust in trusts:
+                                if trust["contact_person_email"] == email:
+                                    if check_password_hash(trust["password"], password):
+                                        return True, "welcome back!"
+                                    else:
+                                        return False, "Incorrect password."
+                                else:
+                                    return False, "No such user found."
 
                     
